6c96b3a461a1f67c68714cd896d854481e15c044
Muhammad Haris <601054+mharis@users.noreply.github.com>
2019-03-04 22:12:26 +0500

    Initial commit.

---
+++ woocommerce-product-addons-hide-dropdown-price.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Product Addons - Hide Price from Dropdown
+ * Plugin URI:
+ * Description: Hides price from dropdown of WooCommerce Product addons.
+ * Version: 1.0.0
+ * Author: Haris Zulfiqar
+ * Author URI: https://hariszulfiqar.com
+ *
+ * @package WooCommerce
+ * @since 1.0.0
+ * @author Haris Zulfiqar <haris@hariszulfiqar.com>
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
